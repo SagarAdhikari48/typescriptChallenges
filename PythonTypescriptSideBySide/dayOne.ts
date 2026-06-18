b4c2f5d6ee3598b7a02e60e2569b56b131719bd3
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-18 11:22:42 +0545

    Day-1- Find largest number in the list

--- PythonTypescriptSideBySide/dayOne.ts
+++ PythonTypescriptSideBySide/dayOne.ts
@@ -9,3 +9,21 @@ function findOddOrEvenNumber(number: number): string {
 
 console.log(findOddOrEvenNumber(10));
 console.log(findOddOrEvenNumber(9));
+
+// # Challenge #2
+// # Find the largest Number from an array
+
+function findLargestNumberInArray(numbers: number[]): number | undefined {
+  let largest = numbers[0];
+  for (const num of numbers) {
+    if (num > largest!) {
+      largest = num;
+    }
+  }
+  return largest;
+}
+
+console.log(
+  "largest number is: ",
+  findLargestNumberInArray([1, 3, 6, 9, 40, 36]),
+);
